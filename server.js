0c2d25a066ad9cd3fe58dd03d16b04d09faa2544
Woji <wojciechpiskorz@gmail.com>
2024-01-04 18:19:50 +0100

    working demo with users from placeholder

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import fetch from "node-fetch";
+
 
 const app = express();
 
@@ -11,6 +13,26 @@ app.use(express.urlencoded({ extended: true }));
 //Parse JSON bodies (as sent by API clients)
 
 app.use(express.json());
+
+// Handle GET request to fetch users
+
+app.get("/users", async (req, res) => {
+  // const users = [
+  //   { id: 1, name: "John Doe" },
+  //   { id: 2, name: "Jane Smith" },
+  //   { id: 3, name: "Josh Adult" },
+  // ];
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  res.send(`
+  <h1 class="text-2xl text-center">Users</h1>
+  <ul>
+    ${users.map((user) => `<li>${user.name}</li>`).join("")}
+  </ul>
+  `)
+});
 
 // start the server
 
